f59291db90468a964a9652cb40725d38e64e5e03
Jen <jenniferlynnebarry@gmail.com>
2022-04-27 16:06:53 -0700

    passing tests

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+var express = require('express');
+var morgan = require('morgan');
+var data = require('./data.json');
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.status(200).send("It's working")
+});
+app.get('/data', (req, res) => {
+    res.status(200).send(data)
+});
 
 // finally export the express application
 module.exports = app;
